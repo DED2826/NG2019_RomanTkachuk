5b00b0d8cd83e7cc0add5076aeb5c5e3fbfbdf7a
Tkachuk Roman <tkachukroman64@gmail.com>
2019-09-15 22:22:17 +0300

    Added Lesson_1

---
+++ Lesson_1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+// cout << "privet"; --- vivedi na ekran
+// cout << endl; --- pereidi na novuy stroku
+// cin >> a; --- polozhi chto-to v a
+// int a; --- sozdat yaschichek a
+// if ( uslovie ) { kod; kod; kod; }
+
+int main()
+{
+}
